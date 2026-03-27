84be66e3f20d99b86e722e46af9dba2778ddaea5
SCSPL Research <riley@scsplresearch.org>
2026-03-27 09:11:17 +0000

    updates

--- main.py
+++ main.py
@@ -1,1 +1,5 @@
-# go where productions take you.proper training first (learn how to construct the parts *properly*). piece-by-piece. nothing rushed. absolute eternal stability for *other men*
+# go where productions take you.
+
+# proper training first (learn how to construct the parts *properly*)
+
+# piece-by-piece. nothing rushed. absolute eternal stability for *other men* to see *other men* thrive
